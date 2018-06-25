e07c1b90433d598f97f149dfa9d95391c77f348b
RyanRio <thermitchellman@gmail.com>
2018-06-25 16:29:05 -0400

    removed useless for statements in favor of list functions... no swearing allowed

--- main.js
+++ main.js
@@ -41,7 +41,17 @@ var Discord = require("discord.js");
 var auth = JSON.parse(fs.readFileSync("./auth.json").toString());
 var bot = new Discord.Client();
 var people = [];
-bot.on("ready", function (evt) {
+bot.on("ready", function () {
+    var wGuild = bot.guilds.get("432343677759651841");
+    if (wGuild) {
+        var wChannel = wGuild.channels
+            .filter(function (wGuild) { return wGuild.type === "text" &&
+            wGuild.permissionsFor(wGuild.client.user).has("SEND_MESSAGES"); }).get("460410488233132042");
+        if (wChannel) {
+            wChannel.send("Hello! " + bot.emojis.find("name", "monkaCozy"));
+        }
+    }
+    people = JSON.parse(fs.readFileSync("recallPeople.json").toString());
 });
 bot.on("messageDelete", function (message) {
     if (channel) {
@@ -53,7 +63,7 @@ var obj = {};
 var channel;
 var channelAssigned = false;
 bot.on("message", function (message) { return __awaiter(_this, void 0, void 0, function () {
-    var args, cmd, rest, emojiList, res;
+    var args, cmd, rest, emojiList, res, msg, initialClass, postEdit, foundClass, me, swearFound_1, swearList;
     return __generator(this, function (_a) {
         if (message.content[0] == "+") {
             args = message.content.substring(1).split(' ');
@@ -101,14 +111,212 @@ bot.on("message", function (message) { return __awaiter(_this, void 0, void 0, f
             }
             if (cmd == "addClasses") {
                 console.log("check2");
-                res = parse(rest, message.author);
+                res = buildFromParse(rest, message.author);
                 message.channel.send("You are taking: " + JSON.stringify(res));
             }
+            if (cmd == "editClass") {
+                msg = rest.split("=>");
+                initialClass = parse(msg[0]);
+                postEdit = parse(msg[1]);
+                foundClass = lookForReference(initialClass, message.author);
+                if (foundClass !== undefined) {
+                    foundClass.classNumber = postEdit.classNumber;
+                    foundClass.section = postEdit.section;
+                    foundClass.type = postEdit.type;
+                }
+            }
+            if (cmd == "save") {
+                fs.writeFileSync("recallPeople.json", JSON.stringify(people));
+                message.channel.send("Data saved...");
+            }
+            if (cmd === "myClasses") {
+                me = people.find(function (person) { return person.id === message.author.id; });
+                if (me) {
+                    message.channel.send(JSON.stringify(me));
+                }
+            }
+            if (cmd === "stop") {
+                message.channel.send("OK shutting down... " + bot.emojis.find("name", "FeelsSobMan"));
+                bot.destroy();
+            }
+        }
+        else {
+            swearFound_1 = false;
+            swearList = ["bitch", "fuck", "ass", "dumbass", "shit", "bullshit", "hell"];
+            swearList.forEach(function (value) {
+                var location = message.content.search(value);
+                if (location !== -1) {
+                    if (location === 0) {
+                        if (message.content[value.length + 1] === " " || message.content.length === value.length) {
+                            swearFound_1 = true;
+                        }
+                    }
+                    else if ((message.content[location - 1] === " " && message.content[location + 1] === " ") || (message.content[location - 1] === " " && message.content.length - location - value.length === 0)) {
+                        swearFound_1 = true;
+                    }
+                }
+            });
+            if (swearFound_1) {
+                message.delete();
+                message.channel.send("No swearing in this good doggy channel!");
+            }
+            /*
+            Disgusting old code before I remembered that a search function existed
+            let index = 0
+            let swearFound = {exists:false, type: "null"}
+        
+        
+            while(index < message.content.length) {
+              const cChar = message.content.charAt(index)
+              switch(cChar) {
+                case "s": {
+                  if(index===0) {
+                    if(message.content.substring(index, index + 5)==="shit ") {
+                      swearFound.exists = true
+                      swearFound.type = "s"
+                      message.delete()
+                      message.channel.send("no swearing on this good christian server")
+                    }
+                    else if(message.content.substring(index, index + 4)==="shit" && message.content.length === index + 4) {
+                      swearFound.exists = true
+                      swearFound.type = "f"
+                      message.delete()
+                      message.channel.send("no swearing on this good christian server")
+                    }
+                  }
+                  else if(message.content.substring(index-1, index + 5)===" shit ") {
+                    swearFound.exists = true
+                    swearFound.type = "s"
+                    message.delete()
+                    message.channel.send("no swearing on this good christian server")
+                  }
+                  break
+                }
+                case "f": {
+                  if(index===0) {
+                    if(message.content.substring(index, index + 5)==="fuck ") {
+                      swearFound.exists = true
+                      swearFound.type = "f"
+                      message.delete()
+                      message.channel.send("no swearing on this good christian server")
+                    }
+                    else if(message.content.substring(index, index + 4)==="fuck" && message.content.length === index + 4) {
+                      swearFound.exists = true
+                      swearFound.type = "f"
+                      message.delete()
+                      message.channel.send("no swearing on this good christian server")
+                    }
+                  }
+                  else if(message.content.substring(index-1, index + 5)===" fuck ") {
+                    swearFound.exists = true
+                    swearFound.type = "f"
+                    message.delete()
+                    message.channel.send("no swearing on this good christian server")
+                  }
+                  break
+                }
+                case "a": {
+                  if(index===0) {
+                    console.log(`"${message.content}"`)
+                    if(message.content.substring(index, index + 4)==="ass ") {
+                      swearFound.exists = true
+                      swearFound.type = "a"
+                      message.delete()
+                      message.channel.send("no swearing on this good christian server")
+                    }
+                    else if(message.content.substring(index, index + 3)==="ass" && message.content.length === index + 3) {
+                      swearFound.exists = true
+                      swearFound.type = "a"
+                      message.delete()
+                      message.channel.send("no swearing on this good christian server")
+                    }
+                  }
+                  else if(message.content.substring(index-1, index + 4)===" ass ") {
+                    swearFound.exists = true
+                    swearFound.type = "a"
+                    message.delete()
+                    message.channel.send("no swearing on this good christian server")
+                  }
+                  break
+                }
+                case "b": {
+                  if(index===0) {
+                    if(message.content.substring(index, index + 6)==="bitch ") {
+                      swearFound.exists = true
+                      swearFound.type = "b"
+                      message.delete()
+                      message.channel.send("no swearing on this good christian server")
+                    }
+                    else if(message.content.substring(index, index + 5)==="bitch" && message.content.length === index + 5) {
+                      swearFound.exists = true
+                      swearFound.type = "b"
+                      message.delete()
+                      message.channel.send("no swearing on this good christian server")
+                    }
+                  }
+                  else if(message.content.substring(index-1, index + 6)===" bitch ") {
+                    swearFound.exists = true
+                    swearFound.type = "b"
+                    message.delete()
+                    message.channel.send("no swearing on this good christian server")
+                  }
+                  break
+                }
+                case "g": {
+                  let swear1 = "goddamn"
+                  let sLength = "goddamn".length
+                  if(index===0) {
+                    if(message.content.substring(index, index + sLength + 1)===swear1 + " ") {
+                      swearFound.exists = true
+                      message.delete()
+                      message.channel.send("no swearing on this good christian server")
+                    }
+                    else if(message.content.substring(index, index + sLength)===swear1 && message.content.length === index + sLength) {
+                      swearFound.exists = true
+                      message.delete()
+                      message.channel.send("no swearing on this good christian server")
+                    }
+                  }
+                  else if(message.content.substring(index-1, index + sLength)===" " + swear1 + " ") {
+                    swearFound.exists = true
+                    message.delete()
+                    message.channel.send("no swearing on this good christian server")
+                  }
+                  break
+                }
+              }
+              index++
+            }
+            swearFound.exists = false
+            */
         }
         return [2 /*return*/];
     });
 }); });
-function parse(classList, author) {
+/**
+ * returns reference to the class in people
+ * @param initialClass
+ * @param author
+ */
+function lookForReference(initialClass, author) {
+    var asdfClass;
+    var me = people.find(function (person) { return person.id === author.id; });
+    if (me) {
+        asdfClass = me.classes.find(function (sClass) {
+            if (sClass.classNumber === initialClass.classNumber) {
+                if (sClass.section === initialClass.section) {
+                    if (sClass.type === initialClass.type) {
+                        // found it... these 2 extra if statements may be unnecesary, but I don't know if there are duplicate classnumbers or etc.
+                        return true;
+                    }
+                }
+            }
+            return false;
+        });
+    }
+    return asdfClass;
+}
+function buildFromParse(classList, author) {
     var authorID = author.id; //use this to fetch
     console.log("check3");
     var returnCheck = checkIfExists(authorID);
@@ -126,6 +334,9 @@ function parse(classList, author) {
     newPerson.id = authorID;
     // begin parsing... classes need to be of format (*area of study**number* *section*,)**
     var splitClassList = classList.split(",");
+    if (splitClassList[1][0] == " ") {
+        splitClassList = classList.split(", ");
+    }
     console.log("class list: " + splitClassList);
     for (var _i = 0, splitClassList_1 = splitClassList; _i < splitClassList_1.length; _i++) {
         var sClass = splitClassList_1[_i];
@@ -157,6 +368,34 @@ function parse(classList, author) {
     }
     console.log("result: " + JSON.stringify(newPerson));
     return newPerson;
+}
+/**
+ * function for modifying a class later on
+ * @param classList
+ */
+function parse(classList) {
+    var siClass = classList.split(" ");
+    var newClass = {
+        type: "",
+        classNumber: 0,
+        section: 0
+    };
+    newClass.section = parseInt(siClass[1]);
+    var index = 0;
+    while (index < siClass[0].length) {
+        var siClassChar = siClass[0][index];
+        console.log("checking parsing: " + JSON.stringify(siClass[0]) + ", on current char: " + siClassChar);
+        var charInt = parseInt(siClassChar);
+        if (!isNaN(charInt)) {
+            console.log("stopping on int: " + charInt);
+            var classNumber = siClass[0].substring(index);
+            newClass.classNumber = parseInt(classNumber);
+            newClass.type = siClass[0].substring(0, index);
+            break;
+        }
+        index++;
+    }
+    return newClass;
 }
 function checkIfExists(authorID) {
     console.log("check4");
--- main.ts
+++ main.ts
@@ -16,9 +16,24 @@ interface NEUClass {
 
 let bot = new Discord.Client()
 
+
 let people: Person[] = [];
-bot.on("ready", function(evt: any) {
+
+bot.on("ready", function() {
+   let wGuild = bot.guilds.get("432343677759651841")
+  if(wGuild) {
+    let wChannel: any = wGuild.channels
+    .filter(wGuild => wGuild.type === "text" &&
+    wGuild.permissionsFor(wGuild.client.user).has("SEND_MESSAGES")).get("460410488233132042")
+
+    if(wChannel) {
+      wChannel.send("Hello! " + bot.emojis.find("name", "monkaCozy"))
+    }
+  }
+
+  people = JSON.parse(fs.readFileSync("recallPeople.json").toString())
 });
+
 
 bot.on("messageDelete", (message) => {
   if(channel) {
@@ -31,6 +46,7 @@ let channel: any
 let channelAssigned = false;
 bot.on("message", async message => {
 
+  
   if(message.content[0]=="+") {
     
     let args = message.content.substring(1).split(' ');
@@ -85,14 +101,219 @@ bot.on("message", async message => {
 
     if(cmd == "addClasses") {
       console.log("check2")
-      let res = parse(rest, message.author)
+      let res = buildFromParse(rest, message.author)
       message.channel.send("You are taking: " + JSON.stringify(res))
     }
 
+    if(cmd == "editClass") {
+      let msg = rest.split("=>")
+      let initialClass = parse(msg[0])
+      let postEdit = parse(msg[1])
+      let foundClass = lookForReference(initialClass, message.author)
+      if(foundClass !== undefined) {
+        foundClass.classNumber = postEdit.classNumber
+        foundClass.section = postEdit.section
+        foundClass.type = postEdit.type
+      }
+    }
+
+    if(cmd == "save") {
+      fs.writeFileSync("recallPeople.json", JSON.stringify(people))
+      message.channel.send("Data saved...")
+    }
+
+    if(cmd === "myClasses") {
+      let me = people.find(person => person.id===message.author.id)
+      if(me) {
+        message.channel.send(JSON.stringify(me))
+      }
+    }
+    
+    if(cmd === "stop") {
+      message.channel.send(`OK shutting down... ${bot.emojis.find("name", "FeelsSobMan")}`)
+      bot.destroy()
+    }
   }
+  else {
+    // parse
+    let swearFound = false
+    let swearList = ["bitch", "fuck", "ass", "dumbass", "shit", "bullshit", "hell"]
+    swearList.forEach(value => {
+      let location = message.content.search(value)
+      if(location!==-1) {
+        if(location===0) {
+          if(message.content[value.length+1]===" " || message.content.length===value.length) {
+            swearFound = true
+          }
+        }
+        else if((message.content[location-1]===" " && message.content[location+1]===" ") || (message.content[location-1]===" " && message.content.length-location-value.length===0)) {
+          swearFound = true
+        }
+      }
+    })
+
+    if(swearFound) {
+      message.delete()
+      message.channel.send("No swearing in this good doggy channel!")
+    }
+
+    /*
+    Disgusting old code before I remembered that a search function existed
+    let index = 0
+    let swearFound = {exists:false, type: "null"}
+
+
+    while(index < message.content.length) {
+      const cChar = message.content.charAt(index)
+      switch(cChar) {
+        case "s": {
+          if(index===0) {
+            if(message.content.substring(index, index + 5)==="shit ") {
+              swearFound.exists = true
+              swearFound.type = "s"
+              message.delete()
+              message.channel.send("no swearing on this good christian server")
+            }
+            else if(message.content.substring(index, index + 4)==="shit" && message.content.length === index + 4) {
+              swearFound.exists = true
+              swearFound.type = "f"
+              message.delete()
+              message.channel.send("no swearing on this good christian server")
+            }
+          }
+          else if(message.content.substring(index-1, index + 5)===" shit ") {
+            swearFound.exists = true
+            swearFound.type = "s"
+            message.delete()
+            message.channel.send("no swearing on this good christian server")
+          }
+          break
+        }
+        case "f": {
+          if(index===0) {
+            if(message.content.substring(index, index + 5)==="fuck ") {
+              swearFound.exists = true
+              swearFound.type = "f"
+              message.delete()
+              message.channel.send("no swearing on this good christian server")
+            }
+            else if(message.content.substring(index, index + 4)==="fuck" && message.content.length === index + 4) {
+              swearFound.exists = true
+              swearFound.type = "f"
+              message.delete()
+              message.channel.send("no swearing on this good christian server")
+            }
+          }
+          else if(message.content.substring(index-1, index + 5)===" fuck ") {
+            swearFound.exists = true
+            swearFound.type = "f"
+            message.delete()
+            message.channel.send("no swearing on this good christian server")
+          }
+          break
+        }
+        case "a": {
+          if(index===0) {
+            console.log(`"${message.content}"`)
+            if(message.content.substring(index, index + 4)==="ass ") {
+              swearFound.exists = true
+              swearFound.type = "a"
+              message.delete()
+              message.channel.send("no swearing on this good christian server")
+            }
+            else if(message.content.substring(index, index + 3)==="ass" && message.content.length === index + 3) {
+              swearFound.exists = true
+              swearFound.type = "a"
+              message.delete()
+              message.channel.send("no swearing on this good christian server")
+            }
+          }
+          else if(message.content.substring(index-1, index + 4)===" ass ") {
+            swearFound.exists = true
+            swearFound.type = "a"
+            message.delete()
+            message.channel.send("no swearing on this good christian server")
+          }
+          break
+        }
+        case "b": {
+          if(index===0) {
+            if(message.content.substring(index, index + 6)==="bitch ") {
+              swearFound.exists = true
+              swearFound.type = "b"
+              message.delete()
+              message.channel.send("no swearing on this good christian server")
+            }
+            else if(message.content.substring(index, index + 5)==="bitch" && message.content.length === index + 5) {
+              swearFound.exists = true
+              swearFound.type = "b"
+              message.delete()
+              message.channel.send("no swearing on this good christian server")
+            }
+          }
+          else if(message.content.substring(index-1, index + 6)===" bitch ") {
+            swearFound.exists = true
+            swearFound.type = "b"
+            message.delete()
+            message.channel.send("no swearing on this good christian server")
+          }
+          break
+        }
+        case "g": {
+          let swear1 = "goddamn"
+          let sLength = "goddamn".length
+          if(index===0) {
+            if(message.content.substring(index, index + sLength + 1)===swear1 + " ") {
+              swearFound.exists = true
+              message.delete()
+              message.channel.send("no swearing on this good christian server")
+            }
+            else if(message.content.substring(index, index + sLength)===swear1 && message.content.length === index + sLength) {
+              swearFound.exists = true
+              message.delete()
+              message.channel.send("no swearing on this good christian server")
+            }
+          }
+          else if(message.content.substring(index-1, index + sLength)===" " + swear1 + " ") {
+            swearFound.exists = true
+            message.delete()
+            message.channel.send("no swearing on this good christian server")
+          }
+          break
+        }
+      }
+      index++
+    }
+    swearFound.exists = false
+    */
+  }
+
 });
 
-function parse(classList: string, author: Discord.User) {
+/**
+ * returns reference to the class in people
+ * @param initialClass 
+ * @param author 
+ */
+function lookForReference(initialClass: NEUClass, author:Discord.User): NEUClass | undefined {
+  let asdfClass: NEUClass | undefined
+  let me = people.find(person => person.id===author.id)
+  if(me) {
+    asdfClass = me.classes.find(sClass => {
+      if (sClass.classNumber === initialClass.classNumber) {
+        if (sClass.section === initialClass.section) {
+          if (sClass.type === initialClass.type) {
+            // found it... these 2 extra if statements may be unnecesary, but I don't know if there are duplicate classnumbers or etc.
+            return true
+          }
+        }
+      }
+      return false
+    })
+  }
+  return asdfClass
+}
+function buildFromParse(classList: string, author: Discord.User) {
   let authorID = author.id; //use this to fetch
   console.log("check3")
   let returnCheck = checkIfExists(authorID);
@@ -110,6 +331,9 @@ function parse(classList: string, author: Discord.User) {
   newPerson.id = authorID
   // begin parsing... classes need to be of format (*area of study**number* *section*,)**
   let splitClassList = classList.split(",");
+  if(splitClassList[1][0]==" ") {
+    splitClassList = classList.split(", ")
+  }
   console.log(`class list: ${splitClassList}`)
   for(let sClass of splitClassList) {
     let siClass = sClass.split(" ");
@@ -142,6 +366,37 @@ function parse(classList: string, author: Discord.User) {
   }
   console.log("result: " + JSON.stringify(newPerson))
   return newPerson
+}
+
+/**
+ * function for modifying a class later on
+ * @param classList 
+ */
+function parse(classList: string) {
+  let siClass = classList.split(" ");
+  let newClass: NEUClass = {
+    type: "",
+    classNumber: 0,
+    section: 0
+  }
+  newClass.section = parseInt(siClass[1]);
+  let index: number = 0;
+  while(index < siClass[0].length) {
+    let siClassChar = siClass[0][index]
+    console.log("checking parsing: " + JSON.stringify(siClass[0]) + ", on current char: " + siClassChar)
+    let charInt: number = parseInt(siClassChar)
+
+    if(!isNaN(charInt)) {
+      console.log("stopping on int: " + charInt)
+      let classNumber = siClass[0].substring(index)
+      newClass.classNumber = parseInt(classNumber)
+      newClass.type = siClass[0].substring(0, index)
+      break
+    }
+    index++
+  }
+
+  return newClass
 }
 
 function checkIfExists(authorID: string): Person | undefined {
